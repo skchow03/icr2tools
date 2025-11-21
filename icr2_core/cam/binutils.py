fab57d288d6c08c57b85357a1950b52d3f2263a4
SK Chow <62276396+skchow03@users.noreply.github.com>
2025-11-20 20:54:36 -0800

    Support loading camera data from DAT archives

--- icr2_core/cam/binutils.py
+++ icr2_core/cam/binutils.py
@@ -1,13 +1,24 @@
 import struct
 
+
+def _read_int32_bytes(data: bytes):
+    return list(struct.unpack("<" + "i" * (len(data) // 4), data))
+
+
 def read_int32_file(filename):
-    with open(filename, 'rb') as f:
+    with open(filename, "rb") as f:
         data = f.read()
-        return list(struct.unpack('<' + 'i' * (len(data) // 4), data))
+        return _read_int32_bytes(data)
+
+
+def read_int32_bytes(data: bytes):
+    return _read_int32_bytes(data)
+
 
 def write_int32_file(filename, int_list):
-    with open(filename, 'wb') as f:
-        f.write(struct.pack('<' + 'i' * len(int_list), *int_list))
+    with open(filename, "wb") as f:
+        f.write(struct.pack("<" + "i" * len(int_list), *int_list))
+
 
 def chunk(lst, n):
-    return [lst[i:i + n] for i in range(0, len(lst), n)]
+    return [lst[i : i + n] for i in range(0, len(lst), n)]
