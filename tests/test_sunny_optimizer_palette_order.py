e86c76fddc53dabd4800157976a29f4d4dbe7643
SK Chow <62276396+skchow03@users.noreply.github.com>
2026-03-03 22:01:11 -0800

    Fix sunny optimizer dirt slot handling for indices 244/245

--- tests/test_sunny_optimizer_palette_order.py
+++ tests/test_sunny_optimizer_palette_order.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from sunny_optimizer.model import OPTIMIZED_SLOTS, SunnyPaletteOptimizer
+from sunny_optimizer.model import OPTIMIZED_SLOTS, SunnyPaletteOptimizer, optimized_slot_count
 
 
 def test_reorder_optimized_colors_places_neutrals_first_and_sorted() -> None:
@@ -22,7 +22,27 @@ def test_reorder_optimized_colors_places_neutrals_first_and_sorted() -> None:
     colors[2] = blue
     colors[3] = neutral_dark
 
-    reordered = optimizer._reorder_optimized_colors(colors)
+    reordered = optimizer._reorder_optimized_colors(colors, OPTIMIZED_SLOTS)
 
     np.testing.assert_array_equal(reordered[0], neutral_dark)
     np.testing.assert_array_equal(reordered[1], neutral_light)
+
+
+def test_compute_palette_uses_244_245_only_when_dirt_present() -> None:
+    rgb_images = {"a": np.array([[[255, 0, 0]]], dtype=np.uint8)}
+    budgets = {"a": 1}
+    fixed_palette = np.zeros((256, 3), dtype=np.uint8)
+
+    without_dirt = SunnyPaletteOptimizer(rgb_images, budgets, fixed_palette, dirt_present=False)
+    palette_without_dirt = without_dirt.compute_palette()
+
+    with_dirt = SunnyPaletteOptimizer(rgb_images, budgets, fixed_palette, dirt_present=True)
+    palette_with_dirt = with_dirt.compute_palette()
+
+    assert optimized_slot_count(False) == OPTIMIZED_SLOTS
+    np.testing.assert_array_equal(palette_without_dirt[244], np.array([255, 0, 0], dtype=np.uint8))
+    np.testing.assert_array_equal(palette_without_dirt[245], np.array([255, 0, 0], dtype=np.uint8))
+
+    assert optimized_slot_count(True) == OPTIMIZED_SLOTS - 2
+    assert not np.array_equal(palette_with_dirt[244], np.array([255, 0, 0], dtype=np.uint8))
+    assert not np.array_equal(palette_with_dirt[245], np.array([255, 0, 0], dtype=np.uint8))
