ba48f86f1f4fe1e8586d29248eae5ac85b4dede4
SK Chow <62276396+skchow03@users.noreply.github.com>
2026-09-24 21:56:39 -0700

    Show clearance next to racing-line model

--- track_viewer/widget/tabs/lp_tab.py
+++ track_viewer/widget/tabs/lp_tab.py
@@ -161,6 +161,8 @@ class LpTabBuilder:
         generation_tools_layout.addWidget(window._generate_lp_button)
         generation_tools_layout.addWidget(window._candidate_race_button)
         generation_tools_layout.addWidget(window._racing_line_model)
+        generation_tools_layout.addWidget(QtWidgets.QLabel("Clearance"))
+        generation_tools_layout.addWidget(window._racing_line_clearance)
         generation_tools_layout.addWidget(window._minimum_time_button)
         generation_tools_layout.addWidget(window._lp_lap_stats_button)
         generation_tools_layout.addWidget(
