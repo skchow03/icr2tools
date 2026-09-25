2bb8953aede215a3c6f4a2a72b1b96cca348c714
SK Chow <62276396+skchow03@users.noreply.github.com>
2026-09-24 22:40:21 -0700

    Show one optimized-line generator button on LP tab

--- track_viewer/widget/tabs/lp_tab.py
+++ track_viewer/widget/tabs/lp_tab.py
@@ -159,11 +159,7 @@ class LpTabBuilder:
         left_layout.addWidget(generation_tools_label)
         generation_tools_layout = QtWidgets.QHBoxLayout()
         generation_tools_layout.addWidget(window._generate_lp_button)
-        generation_tools_layout.addWidget(window._candidate_race_button)
-        generation_tools_layout.addWidget(window._racing_line_model)
-        generation_tools_layout.addWidget(QtWidgets.QLabel("Clearance"))
-        generation_tools_layout.addWidget(window._racing_line_clearance)
-        generation_tools_layout.addWidget(window._minimum_time_button)
+        generation_tools_layout.addWidget(window._optimized_line_button)
         generation_tools_layout.addWidget(window._lp_lap_stats_button)
         generation_tools_layout.addWidget(
             window._recalculate_lateral_speed_button
